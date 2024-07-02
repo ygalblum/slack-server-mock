03123b3cd85452274fd7cbd3c961a19c36defe7e
Ygal Blum <ygal.blum@gmail.com>
2024-07-02 17:15:13 +0300

    Handler - split handle method into smaller methods
    Signed-off-by: Ygal Blum <ygal.blum@gmail.com>

--- slack_server_mock/servers/http/handler.py
+++ slack_server_mock/servers/http/handler.py
@@ -48,72 +48,75 @@ class SlackHTTPHandler(SimpleHTTPRequestHandler):
         "error": "test_data_not_found",
     }
 
+    def _parse_request_body(self):
+        content_len = int(self.headers.get("Content-Length", 0))
+        post_body = self.rfile.read(content_len)
+        request_body = None
+        if post_body:
+            try:
+                post_body = post_body.decode("utf-8")
+                if post_body.startswith("{"):
+                    request_body = json.loads(post_body)
+                else:
+                    request_body = {k: v[0] for k, v in parse_qs(post_body).items()}
+            except UnicodeDecodeError:
+                pass
+        else:
+            parsed_path = urlparse(self.path)
+            if parsed_path and parsed_path.query:
+                request_body = {k: v[0] for k, v in parse_qs(parsed_path.query).items()}
+        return request_body
+
+    def _handle_authorized_request(self):
+        request_body = self._parse_request_body()
+        match self.path:
+            case "/auth.test":
+                body = {
+                    "ok": True,
+                    "url": "https://xyz.slack.com/",
+                    "team": "Testing Workspace",
+                    "user": "bot-user",
+                    "team_id": "T111",
+                    "user_id": "W11",
+                    "bot_id": "B111",
+                    "enterprise_id": "E111",
+                    "is_enterprise_install": False,
+                }
+            case "/apps.connections.open":
+                port = global_injector.get(Settings).websocket_server.port
+                body = {
+                    "ok": True,
+                    "url": f"ws://0.0.0.0:{port}/link",
+                }
+            case "/api.test":
+                if request_body:
+                    body = {"ok": True, "args": request_body}
+            case "/chat.postMessage":
+                global_injector.get(Actor).message_received(request_body['text'])
+                ts = datetime.timestamp(datetime.now())
+                body = {
+                    "ok": True,
+                    "channel": request_body['channel'],
+                    "ts": ts,
+                    "message": {
+                        "text": request_body['text'],
+                        "type": "message",
+                        "ts": ts
+                    }
+                }
+            case _:
+                body = self._not_found
+        return body
+
     def _handle(self):
         try:
             if self._is_valid_token() and self._is_valid_user_agent():
-                parsed_path = urlparse(self.path)
-
-                len_header = self.headers.get("Content-Length") or 0
-                content_len = int(len_header)
-                post_body = self.rfile.read(content_len)
-                request_body = None
-                if post_body:
-                    try:
-                        post_body = post_body.decode("utf-8")
-                        if post_body.startswith("{"):
-                            request_body = json.loads(post_body)
-                        else:
-                            request_body = {k: v[0] for k, v in parse_qs(post_body).items()}
-                    except UnicodeDecodeError:
-                        pass
-                else:
-                    if parsed_path and parsed_path.query:
-                        request_body = {k: v[0] for k, v in parse_qs(parsed_path.query).items()}
-
-                body = {"ok": False, "error": "internal_error"}
-                if self.path == "/auth.test":
-                    body = {
-                        "ok": True,
-                        "url": "https://xyz.slack.com/",
-                        "team": "Testing Workspace",
-                        "user": "bot-user",
-                        "team_id": "T111",
-                        "user_id": "W11",
-                        "bot_id": "B111",
-                        "enterprise_id": "E111",
-                        "is_enterprise_install": False,
-                    }
-                if self.path == "/apps.connections.open":
-                    port = global_injector.get(Settings).websocket_server.port
-                    body = {
-                        "ok": True,
-                        "url": f"ws://0.0.0.0:{port}/link",
-                    }
-                if self.path == "/api.test" and request_body:
-                    body = {"ok": True, "args": request_body}
-                if self.path == "/chat.postMessage":
-                    global_injector.get(Actor).message_received(request_body['text'])
-                    ts = datetime.timestamp(datetime.now())
-                    body = {
-                        "ok": True,
-                        "channel": request_body['channel'],
-                        "ts": ts,
-                        "message": {
-                            "text": request_body['text'],
-                            "type": "message",
-                            "ts": ts
-                        }
-                    }
+                body = self._handle_authorized_request()
             else:
                 body = self._invalid_auth
-
-            if not body:
-                body = self._not_found
-
             self.send_response(HTTPStatus.OK)
             self._set_common_headers()
             self.wfile.write(json.dumps(body).encode("utf-8"))
-
         except Exception as e:
             self.logger.error(str(e), exc_info=True)
             raise
