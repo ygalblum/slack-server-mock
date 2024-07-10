6b13d71d832e7eb7ca80a76c782a5982cc52e9b3
Ygal Blum <ygal.blum@gmail.com>
2024-07-10 10:30:48 -0400

    Implement conversations.list API
    Read the channels list from a file in the slack server
    Add a handler that returns the list from the slack server
    Register the handler
    Add a setting to set the location of the channels file
    Avoid circular import
    
    Signed-off-by: Ygal Blum <ygal.blum@gmail.com>

--- slack_server_mock/slack_server/slack_server.py
+++ slack_server_mock/slack_server/slack_server.py
@@ -1,7 +1,10 @@
 """ Slack Mock Server """
+import json
+
 from injector import inject, singleton
 
 from slack_server_mock.injector.di import global_injector
+from slack_server_mock.settings.settings import Settings
 from slack_server_mock.servers.http.server import SlackHTTPServer
 from slack_server_mock.servers.websocket.server import SlackWebSocketServer
 
@@ -10,9 +13,20 @@ from slack_server_mock.servers.websocket.server import SlackWebSocketServer
 class SlackServer():
     """ Class to hold and manage all Slack server related objects and operations """
     @inject
-    def __init__(self, http_server: SlackHTTPServer, websocket_server: SlackWebSocketServer) -> None:
+    def __init__(
+        self,
+        settings: Settings,
+        http_server: SlackHTTPServer,
+        websocket_server: SlackWebSocketServer
+    ) -> None:
         self._http_server = http_server
         self._websocket_server = websocket_server
+        self._channels = self._load_channels(settings.slack_server.channels_path)
+
+    @property
+    def channels(self):
+        """ Return the channels list """
+        return self._channels
 
     def start(self):
         """ Start the Slack server """
@@ -23,6 +37,16 @@ class SlackServer():
         """ Stop the Slack server """
         self._http_server.stop()
         self._websocket_server.stop()
+
+    @staticmethod
+    def _load_channels(path):
+        channels = []
+        if path:
+            with open(path, "r", encoding="utf-8") as f:
+                channels = json.load(f)
+        if not isinstance(channels, list):
+            raise ValueError("The content of the channels file is not a JSON array")
+        return channels
 
 
 def start_slack_server():
